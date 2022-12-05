9fedd37cd4bdbb7391b7f7a6e4f4ad3418186582
Youssef Hassane <78090820+Youssef-Hassane@users.noreply.github.com>
2022-12-05 12:42:43 +0200

    fix

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'udagram-api-dev.eba-txe5iczf.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-mjjq4bm8.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
